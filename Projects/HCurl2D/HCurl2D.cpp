6dd4ba48eb60620cb08485117d0d9844d3bfed6f
orlandini <francisco.orlandini@gmail.com>
2016-03-14 15:30:21 -0300

    cleaning up messy code

--- Projects/HCurl2D/HCurl2D.cpp
+++ Projects/HCurl2D/HCurl2D.cpp
@@ -50,7 +50,7 @@ void CreateGMesh(TPZGeoMesh * &gmesh, const int meshType, const REAL hDomain, co
  * @param er relative permittivity of the dielectric
  * @param freq frequency of the plane-wave
  */
-TPZCompMesh *CMesh(TPZGeoMesh *gmesh, int pOrder, STATE (& ur)( const TPZVec<REAL> &),STATE (& er)( const TPZVec<REAL> &), REAL L, REAL theta, REAL lambda,REAL e0, REAL scale);
+TPZCompMesh *CMesh(TPZGeoMesh *gmesh, int pOrder, STATE (& ur)( const TPZVec<REAL> &),STATE (& er)( const TPZVec<REAL> &), REAL L, REAL theta, REAL lambda,REAL e0);
 
 /**
  * @brief Implements permeability of possibly inhomongeneous media
@@ -63,23 +63,6 @@ static inline STATE urSubs(const TPZVec<REAL> &x);
  * @param x spatial coordinates
  */
 static inline STATE erSubs(const TPZVec<REAL> &x);
-
-void FilterOthersFunctions(TPZManVector<long> &active, TPZCompMesh * fcmesh);
-
-void ExportCSV(const TPZFMatrix<STATE> matriz, const char* name);
-
-void solAnal(const TPZVec<REAL> &loc, TPZVec<STATE> &result){
-	REAL x = loc[0];
-	REAL y = loc[1];
-	REAL theta = 0.0;
-	REAL e0 = 1.;
-	REAL lambda = 1550*1e-9;
-	REAL c = 3e8;
-	REAL L = 5*lambda;
-	STATE gamma = imaginary *2.*M_PI*c/lambda*sqrt(M_UZERO*M_EZERO*urSubs(loc)*erSubs(loc));
-	STATE e_analytic = e0 * exp( -1. * gamma * ( (L - x) * cos(theta) + y * sin(theta) ) ) ;
-	result[2] =  e_analytic;
-}
 
 int main(int argc, char *argv[])
 {
@@ -102,13 +85,11 @@ int main(int argc, char *argv[])
 	REAL L = 5*lambda;
 	REAL hDomain = L;
 	REAL wDomain = L;
-  REAL scale = 1.;
 	
 //	REAL w=2.*M_PI*M_C/lambda;
 //	REAL kZero=w*sqrt(M_UZERO*M_EZERO);
   
   int pOrder = 1; //ordem polinomial de aproximacao
-  int dim = 2;
   int xDiv = 2;
   int zDiv = 1;
   
@@ -118,7 +99,7 @@ int main(int argc, char *argv[])
   TPZGeoMesh *gmesh = new TPZGeoMesh();
 	CreateGMesh(gmesh, meshType, hDomain, wDomain,  xDiv, zDiv);
 
-  TPZCompMesh *cmesh = CMesh(gmesh, pOrder, urSubs , erSubs , L , theta , lambda , e0 , scale); //funcao para criar a malha computacional
+  TPZCompMesh *cmesh = CMesh(gmesh, pOrder, urSubs , erSubs , L , theta , lambda , e0); //funcao para criar a malha computacional
 	bool optimizeBandwidth = false;
   TPZAnalysis an(cmesh,optimizeBandwidth);
 
@@ -132,58 +113,27 @@ int main(int argc, char *argv[])
   TPZStepSolver<STATE> step;
   step.SetDirect(ELU); //caso simetrico
   an.SetSolver(step);
-  
-  TPZStack<std::string> scalnames, vecnames;
-  vecnames.Push("absE");//setando para imprimir campoeletrico
-	//vecnames.Push("solAnal");//setando para imprimir campoeletrico
-  std::string plotfile= "../HCurl2DEField.vtk";//arquivo de saida que estara na pasta debug
-  an.DefineGraphMesh(dim, scalnames, vecnames, plotfile);//define malha grafica
-  int postProcessResolution = 2 ;//define resolucao do pos processamento
-  //fim das configuracoes do objeto de analise
-	
-  //  const REAL k0 = 2*M_PI*freq*sqrt(M_UZERO*M_EZERO);
-  //  const REAL L = wDomain ;
+
+
   {
     std::ofstream file("../cmesh.txt");
     cmesh->Print(file);
   }
-  // Applying the filter
-//  TPZManVector<long> ActiveEquations;
-//  FilterOthersFunctions(ActiveEquations,cmesh);
-//  an.StructMatrix()->EquationFilter().Reset();
-//  an.StructMatrix()->EquationFilter().SetActiveEquations(ActiveEquations);
   
   // Resolvendo o Sistema
   std::cout<<"entrando no assemble"<<std::endl;
   an.Assemble();
   TPZFMatrix< std::complex<double> > stiff;
   stiff = *an.Solver().Matrix().operator->();
-  stiff.Print("KPZglobalNed = " , std::cout , EMathematicaInput);
-  std::cout<<"saindo do assemble"<<std::endl;
-  std::cout<<"entrando no solver"<<std::endl;
-  an.Solve();
-  std::cout<<"saindo do solver"<<std::endl;
-  const TPZFMatrix<STATE> solucao=cmesh->Solution();//Pegando o vetor de solucao, alphaj
-  std::string name("../../sol");
-  name.append( std::to_string(meshType) );
-  name.append(".csv");
-  ExportCSV(solucao,name.c_str());
   
+  stiff.Print("KPZglobalNed = " , std::cout , EMathematicaInput);
+  
+  an.Solve();
+  
+  const TPZFMatrix<STATE> solucao=cmesh->Solution();//Pegando o vetor de solucao, alphaj
   
   solucao.Print("solucao");
-//  TPZFMatrix<STATE> sds(solucao);
-//  solucao.Print(std::cout);
 	
-  an.PostProcess(postProcessResolution);//realiza pos processamento*)
-  
-//  for (int i = 0; i < solucao.Rows(); i++) {
-//    
-//    sds.Zero();
-//    sds(i,0) = solucao.GetVal(i,0);
-//    cmesh->Solution() = sds;
-//    an.PostProcess(postProcessResolution);//realiza pos processamento*)
-//  }
-  
   timer.stop();
 	
 	
@@ -196,27 +146,6 @@ int main(int argc, char *argv[])
 	std::cout << "FINISHED!" << std::endl;
   
   return 0;
-}
-
-void FilterOthersFunctions(TPZManVector<long> &active, TPZCompMesh * fcmesh){
-  active.Resize(0, 0);
-  int ncon = fcmesh->NConnects();
-  
-  // DOF related with the Q system
-  for(int i = 0; i < ncon; i++)
-  {
-    TPZConnect &con = fcmesh->ConnectVec()[i];
-    int seqnum = con.SequenceNumber();
-    int pos = fcmesh->Block().Position(seqnum);
-    int blocksize = fcmesh->Block().Size(seqnum);// Just for quads
-    
-    int vs = active.size();
-    active.Resize(vs+blocksize);
-    for(int ieq = 0; ieq<blocksize; ieq++)
-    {
-      active[vs+ieq] = pos+ieq;
-    }
-  }
 }
 
 void CreateGMesh(TPZGeoMesh * &gmesh, const int meshType, const REAL hDomain, const REAL wDomain,  const int xDiv, const int zDiv)
@@ -338,7 +267,7 @@ void CreateGMesh(TPZGeoMesh * &gmesh, const int meshType, const REAL hDomain, co
 	delete gengrid;
 }
 
-TPZCompMesh *CMesh(TPZGeoMesh *gmesh, int pOrder, STATE (& ur)( const TPZVec<REAL> &),STATE (& er)( const TPZVec<REAL> &), REAL L, REAL theta, REAL lambda, REAL e0, REAL scale)
+TPZCompMesh *CMesh(TPZGeoMesh *gmesh, int pOrder, STATE (& ur)( const TPZVec<REAL> &),STATE (& er)( const TPZVec<REAL> &), REAL L, REAL theta, REAL lambda, REAL e0)
 {
   
   const int dim = 2; //dimensao do problema
@@ -347,7 +276,7 @@ TPZCompMesh *CMesh(TPZGeoMesh *gmesh, int pOrder, STATE (& ur)( const TPZVec<REA
   const int bc1 = -2; //define id para um material(cond contorno mista)
   enum{ dirichlet = 0, neumann, mixed}; //tipo da condicao de contorno do problema
   // Criando material
-  TPZMatHCurl2D *material = new TPZMatHCurl2D(matId , lambda , ur , er , e0 , theta, scale);//criando material que implementa a
+  TPZMatHCurl2D *material = new TPZMatHCurl2D(matId , lambda , ur , er , e0 , theta);//criando material que implementa a
   //formulacao fraca do problema de validacao
   
   ///set forcing function for L2 projection of desired solution
@@ -435,21 +364,5 @@ inline STATE erSubs(const TPZVec<REAL> &x)
 {
 	//return 4.26 - imaginary * 0.6;
 	return ( 4.+(2.-imaginary*0.1)*(1.-x[0]/(5*1550*1e-9))*(1.-x[0]/(5*1550*1e-9)) );
-}
-void ExportCSV(const TPZFMatrix<STATE> matriz, const char* name)
-{
-  std::ofstream file;
-  if(name!=NULL) file.open(name);
-  
-  for (int i = 0 ; i < matriz.Rows(); i++) {
-    for(int j = 0 ; j < matriz.Cols() ; j++){
-      file<<matriz.GetVal(i, j);
-      if (j != matriz.Cols() - 1) {
-        file<<" , ";
-      }
-    }
-    file<<std::endl;
-  }
-  file.close();
 }
 
