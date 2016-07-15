a1eff242a09f9b8d12f140ec0d599c70d53f23a8
philippedevloo <philippedevloo1958@gmail.com>
2016-07-15 10:15:14 +0200

    Revert "The order of a corner connect is 1"
    This reverts commit 0ed58bb24d3fe6b0d1ccd3c90704cbc3d7b57a26.

--- Mesh/pzintel.cpp
+++ Mesh/pzintel.cpp
@@ -239,12 +239,9 @@ void TPZInterpolatedElement::IdentifySideOrder(int side)
     elvecall.Push(thisside);
     thisside.EqualLevelElementList(elvecall,1,0);
 	int i;
-    // Verify is the side order of all elements is equal to neworder
-    long connectindex = ConnectIndex(MidSideConnectLocId(side));
-    
 	for(i=0; i<elvecall.NElements(); i++)
 	{
-		if(elvecall[i].ConnectIndex() != -1 && elvecall[i].ConnectIndex() == connectindex) elvecequal.Push(elvecall[i]);
+		if(elvecall[i].ConnectIndex() != -1) elvecequal.Push(elvecall[i]);
 	}
 	
 	TPZInterpolatedElement *equal;
@@ -292,7 +289,7 @@ void TPZInterpolatedElement::IdentifySideOrder(int side)
             DebugStop();
 			large = thisside.LowerLevelElementList(1);
 		}
-        /// compare the fPreferred Orders of the neighbouring elements
+        /// comparte the fPreferred Orders of the neighbouring elements
 		neworder = ComputeSideOrder(elvecequal);
 		// Verify is the side order of all elements is equal to neworder
         long connectindex = ConnectIndex(MidSideConnectLocId(side));
