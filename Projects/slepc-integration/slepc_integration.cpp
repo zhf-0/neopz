41c50689f6ace69c8d63b8ec7ec4392cbe95a97d
Francisco Orlandini <francisco.orlandini@gmail.com>
2017-11-30 17:33:23 -0200

    First time passing matrix to petsc

--- Projects/slepc-integration/slepc_integration.cpp
+++ Projects/slepc-integration/slepc_integration.cpp
@@ -1,196 +1,29 @@
-/*
-   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   SLEPc - Scalable Library for Eigenvalue Problem Computations
-   Copyright (c) 2002-2016, Universitat Politecnica de Valencia, Spain
 
-   This file is part of SLEPc.
+#include <pzsespmat.h>
+#include <pzsysmp.h>
+#include "petscmat.h"
+static char help[] = "Testing MatCreateSeqSBAIJWithArrays().\n\n";
+int main(int argc, char **argv){
+  const int nRows = 3;
+  const int nCols = nRows;
+  TPZSYsmpMatrix<STATE> matrix(nRows,nCols);
+  TPZVec<long> ia ({0,2,3,4});
+  TPZVec<long> ja({0,2,1,2});
 
-   SLEPc is free software: you can redistribute it and/or modify it under  the
-   terms of version 3 of the GNU Lesser General Public License as published by
-   the Free Software Foundation.
+  TPZVec<STATE> aVec({1.,3.,4.,5.});
+  matrix.SetData(ia,ja,aVec);
+  matrix.Print("mat");
+  const int blockSize = 1;
+  Mat A;
+  //@TODO:Think on int/long issue
+  //@TODO:Think on PetsScalar issue
+  TPZVec<int> iaP ({0,2,3,4});
+  TPZVec<int> jaP({0,2,1,2});
+  TPZVec<std::complex<double>> aVecP({1.,3.,4.,5.});
 
-   SLEPc  is  distributed in the hope that it will be useful, but WITHOUT  ANY
-   WARRANTY;  without even the implied warranty of MERCHANTABILITY or  FITNESS
-   FOR  A  PARTICULAR PURPOSE. See the GNU Lesser General Public  License  for
-   more details.
-
-   You  should have received a copy of the GNU Lesser General  Public  License
-   along with SLEPc. If not, see <http://www.gnu.org/licenses/>.
-   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-*/
-
-static char help[] = "Standard symmetric eigenproblem corresponding to the Laplacian operator in 1 dimension.\n\n"
-        "The command line options are:\n"
-        "  -n <n>, where <n> = number of grid subdivisions = matrix dimension.\n\n";
-
-#include <slepceps.h>
-
-#undef __FUNCT__
-#define __FUNCT__ "main"
-int main(int argc,char **argv)
-{
-  Mat            A;           /* problem matrix */
-  EPS            eps;         /* eigenproblem solver context */
-  EPSType        type;
-  PetscReal      error,tol,re,im;
-  PetscScalar    kr,ki;
-  Vec            xr,xi;
-  PetscInt       n=30,i,Istart,Iend,nev,maxit,its,nconv;
-  PetscErrorCode ierr;
-
-  SlepcInitialize(&argc,&argv,(char*)0,help);
-
-  ierr = PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);
-  CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD,"\n1-D Laplacian Eigenproblem, n=%D\n\n",n);
-  CHKERRQ(ierr);
-
-  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-     Compute the operator matrix that defines the eigensystem, Ax=kx
-     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
-  ierr = MatCreate(PETSC_COMM_WORLD,&A);
-  CHKERRQ(ierr);
-  ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,n,n);
-  CHKERRQ(ierr);
-  ierr = MatSetFromOptions(A);
-  CHKERRQ(ierr);
-  ierr = MatSetUp(A);
-  CHKERRQ(ierr);
-
-  ierr = MatGetOwnershipRange(A,&Istart,&Iend);
-  CHKERRQ(ierr);
-  for (i=Istart;i<Iend;i++) {
-    if (i>0) { ierr = MatSetValue(A,i,i-1,-1.0,INSERT_VALUES);
-      CHKERRQ(ierr);
-    }
-    if (i<n-1) { ierr = MatSetValue(A,i,i+1,-1.0,INSERT_VALUES);
-      CHKERRQ(ierr); }
-    ierr = MatSetValue(A,i,i,2.0,INSERT_VALUES);
-    CHKERRQ(ierr);
-  }
-  ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);
-  CHKERRQ(ierr);
-  ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);
-  CHKERRQ(ierr);
-
-  ierr = MatCreateVecs(A,NULL,&xr);
-  CHKERRQ(ierr);
-  ierr = MatCreateVecs(A,NULL,&xi);
-  CHKERRQ(ierr);
-
-  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-                Create the eigensolver and set various options
-     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  /*
-     Create eigensolver context
-  */
-  ierr = EPSCreate(PETSC_COMM_WORLD,&eps);
-  CHKERRQ(ierr);
-
-  /*
-     Set operators. In this case, it is a standard eigenvalue problem
-  */
-  ierr = EPSSetOperators(eps,A,NULL);
-  CHKERRQ(ierr);
-  ierr = EPSSetProblemType(eps,EPS_HEP);
-  CHKERRQ(ierr);
-
-  /*
-     Set solver parameters at runtime
-  */
-  ierr = EPSSetFromOptions(eps);
-  CHKERRQ(ierr);
-
-  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-                      Solve the eigensystem
-     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
-  ierr = EPSSolve(eps);
-  CHKERRQ(ierr);
-  /*
-     Optional: Get some information from the solver and display it
-  */
-  ierr = EPSGetIterationNumber(eps,&its);
-  CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of iterations of the method: %D\n",its);
-  CHKERRQ(ierr);
-  ierr = EPSGetType(eps,&type);
-  CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD," Solution method: %s\n\n",type);
-  CHKERRQ(ierr);
-  ierr = EPSGetDimensions(eps,&nev,NULL,NULL);
-  CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of requested eigenvalues: %D\n",nev);
-  CHKERRQ(ierr);
-  ierr = EPSGetTolerances(eps,&tol,&maxit);
-  CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD," Stopping condition: tol=%.4g, maxit=%D\n",(double)tol,maxit);
-  CHKERRQ(ierr);
-
-  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-                    Display solution and clean up
-     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  /*
-     Get number of converged approximate eigenpairs
-  */
-  ierr = EPSGetConverged(eps,&nconv);
-  CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD," Number of converged eigenpairs: %D\n\n",nconv);
-  CHKERRQ(ierr);
-
-  if (nconv>0) {
-    /*
-       Display eigenvalues and relative errors
-    */
-    ierr = PetscPrintf(PETSC_COMM_WORLD,
-                       "           k          ||Ax-kx||/||kx||\n"
-                               "   ----------------- ------------------\n");
-    CHKERRQ(ierr);
-
-    for (i=0;i<nconv;i++) {
-      /*
-        Get converged eigenpairs: i-th eigenvalue is stored in kr (real part) and
-        ki (imaginary part)
-      */
-      ierr = EPSGetEigenpair(eps,i,&kr,&ki,xr,xi);
-      CHKERRQ(ierr);
-      /*
-         Compute the relative error associated to each eigenpair
-      */
-      ierr = EPSComputeError(eps,i,EPS_ERROR_RELATIVE,&error);
-      CHKERRQ(ierr);
-
-#if defined(PETSC_USE_COMPLEX)
-      re = PetscRealPart(kr);
-      im = PetscImaginaryPart(kr);
-#else
-      re = kr;
-      im = ki;
-#endif
-      if (im!=0.0) {
-        ierr = PetscPrintf(PETSC_COMM_WORLD," %9f%+9fi %12g\n",(double)re,(double)im,(double)error);
-        CHKERRQ(ierr);
-      } else {
-        ierr = PetscPrintf(PETSC_COMM_WORLD,"   %12f       %12g\n",(double)re,(double)error);
-        CHKERRQ(ierr);
-      }
-    }
-    ierr = PetscPrintf(PETSC_COMM_WORLD,"\n");
-    CHKERRQ(ierr);
-  }
-
-  /*
-     Free work space
-  */
-  ierr = EPSDestroy(&eps);
-  CHKERRQ(ierr);
-  ierr = MatDestroy(&A);
-  CHKERRQ(ierr);
-  ierr = VecDestroy(&xr);
-  CHKERRQ(ierr);
-  ierr = VecDestroy(&xi);
-  CHKERRQ(ierr);
-  ierr = SlepcFinalize();
-  return ierr;
+  PetscInitialize(&argc,&argv,(char *)0,help);
+  MatCreateSeqSBAIJWithArrays(MPI_COMM_WORLD,blockSize,nRows,nCols,(PetscInt *)iaP.begin(),(PetscInt *)jaP.begin(),(PetscScalar *)aVecP.begin(),&A);
+  MatView(A, PETSC_VIEWER_STDOUT_WORLD);
+  PetscViewerFlush(PETSC_VIEWER_STDOUT_WORLD); // Diag: 1, 2, 1
+  return 0;
 }
